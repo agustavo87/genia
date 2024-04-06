f5f333956383e31ef9db0dcac8f1f9ccefa8226a
Gustavo R. Ayala <agustavo87@gmail.com>
2024-04-06 20:46:31 -0300

    Throw on HTTP Client error in GetIMG API

--- app/Actions/GetimgGenerateImage.php
+++ app/Actions/GetimgGenerateImage.php
@@ -15,18 +15,21 @@ class GetimgGenerateImage implements GenerateImage
 
     public function handle(string $prompt, string $id): string
     {
+        // Some change
         $response = Http::withHeaders([
             'accept' => 'application/json',
             'authorization' => 'Bearer '.$this->key,
             'content-type' => 'application/json',
             // 'https://api.getimg.ai/v1/stable-diffusion/text-to-image'
-        ])->post('https://api.getimg.ai/v1/stable-diffusion-xl/text-to-image', [
-            'prompt' => $prompt,
-            // "model" => icbinp-seco',
-            // dark-sushi-mix-v2-25, synthwave-punk-v2, openjourney-v4, arcane-diffusion, realistic-vision-v5-1,
-            // moonfilm-utopia-v3, mo-di-diffusion, van-gogh-diffusion, stable-diffusion-v2-1, stable-diffusion-xl-v1-0,
-            'output_format' => 'png',
-        ]);
+        ])
+            ->post('https://api.getimg.ai/v1/stable-diffusion-xl/text-to-image', [
+                'prompt' => $prompt,
+                // "model" => icbinp-seco',
+                // dark-sushi-mix-v2-25, synthwave-punk-v2, openjourney-v4, arcane-diffusion, realistic-vision-v5-1,
+                // moonfilm-utopia-v3, mo-di-diffusion, van-gogh-diffusion, stable-diffusion-v2-1, stable-diffusion-xl-v1-0,
+                'output_format' => 'png',
+            ])
+            ->throwUnlessStatus(200);
 
         /** @var string */
         $base64EncodedImage = $response->json('image');
