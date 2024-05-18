71868e3ac2e418454159c7c69a4b7b6b6758d6ff
Gustavo R. Ayala <agustavo87@gmail.com>
2024-05-18 17:07:53 -0300

    FEAT: Changed app icon

--- resources/views/generate.blade.php
+++ resources/views/generate.blade.php
@@ -2,13 +2,7 @@
     <div class="bg-black text-white flex min-h-screen flex-col items-center pt-16 sm:justify-center sm:pt-0">
         <a href="#">
             <div class="text-foreground font-semibold text-2xl tracking-tighter mx-auto flex items-center gap-2">
-                <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672Zm-7.518-.267A8.25 8.25 0 1 1 20.25 10.5M8.288 14.212A5.25 5.25 0 1 1 17.25 10.5" />
-                    </svg>
-                </div>
+                <x-icons.magic class="w-6 h-6" />
                 GenIA
             </div>
         </a>
